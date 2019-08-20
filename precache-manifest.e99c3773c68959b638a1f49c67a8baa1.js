db5dc4e5f5aa31d72be3a28c0f13919d2e8c951d
unknown <mariia>
2019-08-20 04:29:59 +0300

    Updates

---
+++ precache-manifest.e99c3773c68959b638a1f49c67a8baa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1900dfe459dbe4e81d30518c30eee9b9",
+    "url": "/DevelopsToday-test/index.html"
+  },
+  {
+    "revision": "e5882fc4ccc2973c8fef",
+    "url": "/DevelopsToday-test/static/css/main.c83268e5.chunk.css"
+  },
+  {
+    "revision": "28edf9104b7c265d7492",
+    "url": "/DevelopsToday-test/static/js/2.76c2d923.chunk.js"
+  },
+  {
+    "revision": "e5882fc4ccc2973c8fef",
+    "url": "/DevelopsToday-test/static/js/main.58854793.chunk.js"
+  },
+  {
+    "revision": "afe99f3c4ac38e2f0aca",
+    "url": "/DevelopsToday-test/static/js/runtime~main.28d0552a.js"
+  }
+]);
