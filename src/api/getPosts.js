fea1994d5008ea42b2a8e98bcb74fcf79208c184
Mariya Hubko <48643833+mariyahubko04@users.noreply.github.com>
2019-08-20 15:35:24 +0300

    Delete getPosts.js

--- src/api/getPosts.js
+++
@@ -1,8 +0,0 @@
-export const getPosts = async() => {
-  const url = 'https://simple-blog-api.crew.red/posts';
-
-  const response = await fetch(url);
-  const posts = await response.json();
-
-  return posts;
-};
